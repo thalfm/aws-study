80f707d6f2fb25aa6ec3c48cb2795ea5adfb78fa
Cleber Santos <cleber.santos@grancursosonline.com.br>
2023-04-12 21:52:14 -0300

    chore: proximos passos
    Co-authored-by: Thales Martins <thalfm@gmail.com>
    Co-authored-by: Lucas Vieira <lucas.vieira789@hotmail.com>

---
+++ cloudformation/scripts/10-criar-stack-ecs.sh
@@ -0,0 +1,18 @@
+
+## @TODO
+
+# criar repositorio
+
+#buildar imagem
+
+#subir imagem para o ECR
+
+# criar vpc
+
+# criar subnet privada na vpc
+
+# criar cluster
+
+# criar task definition
+
+# criar lambda para rodar a task
