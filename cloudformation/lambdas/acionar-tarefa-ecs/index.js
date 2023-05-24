c4a11c8b4e1bb6739607e00bd0fe90b9ceadbfcd
lucas <lucas.vieira@grancursosonline.com.br>
2023-05-24 20:45:29 -0300

    feat:wip: Inclui configuração das variaveis na lambda
    Co-authored-by: Thales Martins <thalfm@gmail.com>
    Co-authored-by: Lucas Vieira <oclebersantos@gmail.com>

--- cloudformation/lambdas/acionar-tarefa-ecs/index.js
+++ cloudformation/lambdas/acionar-tarefa-ecs/index.js
@@ -3,71 +3,79 @@ const AWS = require('aws-sdk');
 const ecs = new AWS.ECS();
 
 const environment = {
-  taskconfiguration: {
-    cluster: env.get('ECS_CLUSTER_NAME').required().asString(),
-    launchType: env.get('ECS_TASK_LAUNCH_TYPE').required().asString(),
-    count: env.get('ECS_TASK_COUNT').required().asString(),
-    taskDefinition: env.get('ECS_TASK_DEFINITION').required().asString(),
-    platformVersion: env.get('ECS_TASK_PLATFORM_VERSION').required().asString(),
-  },
-  containerConfiguration: {
-    name: env.get('ECS_TASK_CONTAINER_NAME').required().asString(),
-    fileEnvName: env.get('ECS_TASK_CONTAINER_FILE_ENV_NAME').required().asString(),
-  },
-  network: {
-    subnets: env.get('ECS_TASK_SUBNETS').required().asArray(),
-    securityGroups: env.get('ECS_SECURITY_GROUPS').required().asArray(),
-    assignPublicIp: env.get('ECS_TASK_ASSIGN_PUBLIC_IP').required().asString(),
-  }
+    taskconfiguration: {
+        cluster: env.get('ECS_CLUSTER_NAME').required().asString(),
+        launchType: 'FARGATE',
+        count: 1,
+        taskDefinition: env.get('ECS_TASK_DEFINITION').required().asString(),
+        platformVersion: 'LATEST',
+    },
+    containerConfiguration: {
+        name: 'integracao-erp'
+    },
+    network: {
+        subnets: env.get('ECS_TASK_SUBNETS').required().asArray(),
+        securityGroups: env.get('ECS_SECURITY_GROUPS').required().asArray(),
+        assignPublicIp: "ENABLED",
+    }
 }
 
 const scheduleEcs = async (data, environment) => {
-  return ecs.runTask({
-    ...environment.taskconfiguration,
-    overrides: {
-      containerOverrides: [
-        {
-          name: environment.containerConfiguration.name,
-          environment: [
-            {
-              name: environment.containerConfiguration.fileEnvName,
-              value: data
+    return ecs.runTask({
+        ...environment.taskconfiguration,
+        overrides: {
+            containerOverrides: [
+                {
+                    name: environment.containerConfiguration.name,
+                    environment: [
+                        {
+                            name: 'ORDERS_CREATED',
+                            value: data
+                        },
+                        {
+                            name: 'AWS_ACCESS_KEY',
+                            value: env.get('AWS_ACCESS_KEY').required().asString(),
+                        },
+                        {
+                            name: 'AWS_SECRET_KEY',
+                            value: env.get('AWS_SECRET_KEY').required().asString(),
+                        },
+                        {
+                            name: 'AWS_SQS_URL_ORDERS_SEND',
+                            value: env.get('AWS_SQS_URL_ORDERS_SEND').required().asString(),
+                        }
+                    ]
+                }
+            ],
+        },
+        networkConfiguration: {
+            awsvpcConfiguration: {
+                ...environment.network
             }
-          ]
         }
-      ],
-    },
-    networkConfiguration: {
-      awsvpcConfiguration: {
-        ...environment.network
-      }
-    }
-  }).promise()
+    }).promise()
 }
 
 exports.handler = async (event, context) => {
 
     const message = event.Records[0].body;
 
-    const params = {
-      MessageBody: message,
-      QueueUrl: queueUrl
-    };
+    const params = JSON.stringify(message);
 
-    console.log("scheduling with sqs data...", JSON.stringify(params))
+    console.log("scheduling with sqs data...", params)
 
-    const result = await scheduleEcs(JSON.stringify(params), environment)
+    const result = await scheduleEcs(params, environment)
 
     console.log("result", result);
 
     return {
-      statusCode: 200,
-      body: JSON.stringify(
-          {
-            result
-          },
-          null,
-          2
-      )
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                result
+            },
+            null,
+            2
+        )
     };
 };
