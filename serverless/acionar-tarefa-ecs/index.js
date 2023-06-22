3aad1add8be08ab935b9279de2c09ecefe950a5a
Cleber Santos <cleber.santos@grancursosonline.com.br>
2023-06-21 23:02:58 -0300

    feat: Cria lambda acionar-tarefa-ecs utilizando o Serverless
    Co-authored-by: Thales Martins <thalfm@gmail.com>
    Co-authored-by: Lucas Vieira <lucas.vieira789@hotmail.com>

---
+++ serverless/acionar-tarefa-ecs/index.js
@@ -0,0 +1,15 @@
+
+const consumer = async (event) => {
+  for (const record of event.Records) {
+    const messageAttributes = record.messageAttributes;
+    console.log(
+      "Message Attribute: ",
+      messageAttributes.AttributeName.stringValue
+    );
+    console.log("Message Body: ", record.body);
+  }
+};
+
+module.exports = {
+  consumer,
+};
